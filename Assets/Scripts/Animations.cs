1676927200cf9d40bebc0e6689bc16bb5a7fe144
LandersGFX <55904937+LandersGFX@users.noreply.github.com>
2021-05-28 15:15:25 -0500

    File Uploads

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    public Sprite[] spriteArray;
+    public int currentSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        ChangeSprite();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ChangeSprite()
+    {
+        for (int i = 0; i <= spriteArray.Length; i++)
+        {
+            spriteRenderer.sprite = spriteArray[i];
+            currentSprite++;
+            if (currentSprite >= spriteArray.Length)
+            {
+                currentSprite = 0;
+                i = 0;
+            }
+
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody2d;
+    float xmovement;
+    
+    public float jumpForce = 20f;
+    bool isGrounded;
+    public Transform GroundHitbox;
+    public LayerMask groundLayers;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        xmovement = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(xmovement * speed, rigidbody2d.velocity.y);
+
+        rigidbody2d.velocity = movement;
+    }
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rigidbody2d.velocity.x, jumpForce);
+        rigidbody2d.velocity = movement;
+    }
+    public bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(GroundHitbox.position, 0.5f, groundLayers);
+        if (groundCheck.gameObject != null){
+            return true;
+        }
+        return false;
+    }
+}
